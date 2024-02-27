630d7a04c13ec1ceaba176871a4fbcfe9f1b783e
Fernanda Sasso <fsasso.23@gmail.com>
2024-02-27 15:44:07 -0300

    perfil/alerta

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
 //import React from 'react';
 import './App.css';
-import Home from './paginas/home/Home';
+
 import Navbar from './components/navBar/NavBar';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './paginas/login/Login';
+import Cadastro from './paginas/cadastro/Cadastro';
+import Home from './paginas/home/Home';
+import { AuthProvider } from './contexts/AuthContext';
+import { ToastContainer } from 'react-toastify';
+import ListaTemas from './components/temas/listaTemas/ListaTemas';
+import FormularioTema from './components/temas/formularioTema/FormularioTema';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+import ListaPostagem from './components/postagem/listaPostagem/ListaPostagem';
+import FormularioPostagem from './components/postagem/formularioPostagem/FormularioPostagem';
+import DeletarPostagem from './components/postagem/deletarPostagem/DeletarPostagem';
+import Perfil from './paginas/perfil/Perfil';
+import 'react-toastify/dist/ReactToastify.css';
+
+const contextClass = {
+  success: "bg-blue-600",
+  error: "bg-red-600",
+  info: "bg-gray-600",
+  warning: "bg-orange-400",
+  default: "bg-indigo-600",
+  colored: "bg-red-200 font-gray-600",
+};
+
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-        <Navbar />
+    <AuthProvider>
+    <ToastContainer toastClassName={(context) =>
+          contextClass[context?.type || "default"] +
+          " relative flex p-1 min-h-10 rounded-md justify-between overflow-hidden cursor-pointer"
+        }
+        bodyClassName={() => "text-sm font-white font-med block p-3"}
+        position="bottom-left"
+        autoClose={3000}/>
+        <BrowserRouter>
+          <Navbar />
           <div className='min-h-[80vh]'>
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagem />} />
+              <Route path="/cadastroPostagem" element={<FormularioPostagem />} />
+              <Route path="/editarPostagem/:id" element={<FormularioPostagem />} />
+              <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+              <Route path="/perfil" element={<Perfil />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
+        </AuthProvider>
     </>
-);
+  );
 }
 export default App;
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,6 +1,7 @@
 //import React from 'react';
 import './Home.css';
-
+import ListaPostagem from '../../components/postagem/listaPostagem/ListaPostagem';
+import ModalPostagem from '../../components/postagem/modalPostagem/ModalPostagem';
 
 function Home() {
     return (
@@ -12,18 +13,18 @@ function Home() {
               <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
   
               <div className="flex justify-around gap-4">
-              
-                <button className='rounded bg-white text-red-800 py-2 px-4'>Ver postagens</button>
+              <ModalPostagem />
+                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
               </div>
             </div>
   
             <div className="flex justify-center ">
-              <img src="https://img.freepik.com/vetores-gratis/ilustracao-japonesa-desenhada-a-mao-de-flores-de-cerejeira-e-fa-japones_23-2149601829.jpg?t=st=1708006484~exp=1708007084~hmac=940b14d28b8ff5f8caba46faa667e6f5969ffdb65eca6b1e21a905fbaa9abe6e" alt="leque" className='w-2/3' />
+              <img src="https://img.freepik.com/vetores-gratis/ilustracao-do-conceito-de-conversa_114360-1102.jpg?w=996&t=st=1708555651~exp=1708556251~hmac=bb0e7069ad7d4ad910aab59675d920a7247621b1d41807900c7baefc3811b9c9" alt="" className='w-2/3' />
       
             </div>
           </div>
         </div>
-      
+        <ListaPostagem />
       </>
     );
 }
